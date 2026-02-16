38950c5fdaf1179cd3edb625f5f121ab35becac6
user <mihao85@gmail.com>
2026-02-16 10:16:30 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,22 +5,10 @@ import java.time.LocalDateTime;
 public class Item {
     private int id;
     private String name;
-    LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime created = LocalDateTime.now();
 
     public LocalDateTime getCreated() {
         return created;
-    }
-
-    public Item() {
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
-    public Item(int id, String name) {
-        this.id = id;
-        this.name = name;
     }
 
     public int getId() {
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,15 +1,13 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
 
     public static void main(String[] args) {
         Item item = new Item();
-        LocalDateTime created = item.getCreated();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String formattedDate = created.format(formatter);
+        String formattedDate = item.getCreated().format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + formattedDate);
     }
 }
